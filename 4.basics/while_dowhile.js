bb96cd8bbf9f17d5a34dae3cb575c80ec3e0a37e
adarshdas13 <adarshdas807@gmail.com>
2026-07-14 19:28:19 +0530

    do-while,while,for,continue,break

---
+++ 4.basics/while_dowhile.js
@@ -0,0 +1,20 @@
+//while
+let i = 1
+while (i <= 10) {
+    // console.log(`value of i: ${i}`);
+    i = i + 2
+}
+
+let heroes = ["groot","hulk","thor","flash"]
+i = 0
+while (i < heroes.length) {
+    // console.log(`heroes are : ${heroes[i]}`);
+    i++
+}
+
+//do while
+let score = 11
+do {
+    console.log(`score: ${score}`);
+    score++
+} while (score <= 10);
