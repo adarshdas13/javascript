7ac53e9492ddf1461ed1c384952e85a306348705
adarshdas13 <adarshdas807@gmail.com>
2026-07-19 22:42:42 +0530

    mouse events

---
+++ 10.dom-event_3/script.js
@@ -0,0 +1,17 @@
+// mouse.addEventListener("mouseover", function(){
+//     mouse.style.backgroundColor = "blue"
+// })
+// mouse.addEventListener("mouseout", function(){
+//     mouse.style.backgroundColor = "yellow"
+// })
+
+//mouse follow up
+// let mouse = document.querySelector(".mouse");
+
+// window.addEventListener("mousemove", function(dets){
+//     mouse.style.left = dets.clientX + "px";
+//     mouse.style.top = dets.clientY + "px";
+// });
+
+//dets - event obj
+
