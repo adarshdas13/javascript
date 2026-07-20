9f7ee6361615e0c61841cc672207a03de4e93a2c
adarshdas13 <adarshdas807@gmail.com>
2026-07-20 12:43:21 +0530

    added event bubbling,capturing,char count,strike design

--- 10.dom-event_3/script.js
+++ 10.dom-event_3/script.js
@@ -15,3 +15,20 @@
 
 //dets - event obj
 
+//event bubbling - if an element does not have a event listerner it will go check its parent for a event listerner if present it will be executed if not it goes to the main element
+// document.querySelector(".nav").addEventListener("click",function (){
+//     alert("clicked")
+// })
+
+//strike through design
+// let ul = document.querySelector("ul")
+// ul.addEventListener("click",function (dets){
+//     dets.target.classList.toggle("lt")
+// })
+
+//character count
+let inp = document.querySelector("input")
+let span = document.querySelector("span")
+inp.addEventListener("input",function (){
+    span.textContent = inp.value.length
+})
